b24c593283dca1b18681b8770bab74d718a9de75
Isis <83175091+1sis@users.noreply.github.com>
2024-02-01 16:00:50 +0100

    [REFACTO] getHash.py (again)

--- src/modules/scan/getHash.py
+++ src/modules/scan/getHash.py
@@ -1,39 +1,29 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.1
 # coding:utf-8
 
 import hashlib
 import os
 from typing import List
 
-class getFileHash:
-
-    # Constants
+class FileHash(object):
     BUF_SIZE: int = 4096
 
-    @staticmethod
-    # Get SHA-256 hash of a file
-    def get_hash_file(file_path: str) -> str:
+    def get_hash(self, path: str) -> str:
+        """
+           Gets hash for a single file
+        """
+
         hash_value = hashlib.sha256()
-        with open(file_path, "rb") as file:
-            for byte_block in iter(lambda: file.read(getFileHash.BUF_SIZE), b""):
+        with open(path, "rb") as file:
+            for byte_block in iter(lambda: file.read(self.BUF_SIZE), b""):
                 hash_value.update(byte_block)
         return hash_value.hexdigest()
 
+    def get_file_hashes(self, dir: str) -> List[str]:
+        """
+          Get SHA-256 hashes of files in a directory
+        """
+        if not os.path.isdir(dir):
+            raise ValueError(f"{dir} is not a valid directory")
 
-    @staticmethod
-    # Get SHA-256 hashes of files in a directory
-    def get_file_from_directory(directory_path: str) -> List[str]:
-        hashes: List[str] = []
-        if os.path.isdir(directory_path):
-            # Iterate over files in the directory
-            for file_name in os.listdir(directory_path):
-                file_path = os.path.join(directory_path, file_name)
-                # Check if the path is a file
-                if os.path.isfile(file_path):
-                    # Calculate hash for each file
-                    sha256_hash = getFileHash.get_hash(file_path)
-                    print(f"[+] SHA256 hash of {file_name}: {sha256_hash}")
-                    hashes.append(sha256_hash)
-        else:
-            print("[-] Directory not found!")
-        return hashes
+        return [self.get_hash(os.path.join(dir, x)) for x in os.listdir(dir)]
