6f0e437cc2fc78b35c810dc7ce0ed8f0137d8a21
1sis <83175091+1sis@users.noreply.github.com>
2024-02-29 23:24:42 +0100

    refacto compare_hash

--- src/database/import_hash.py
+++ src/database/import_hash.py
@@ -5,7 +5,7 @@ client = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
 db = client["hashes_database"]
 collection = db["hash_collection"]
 
-with open("../src/database/hashes.txt", "r") as file:
+with open("hashes.txt", "r") as file:
     hashes = file.readlines()
 
 for hash_str in hashes:
--- src/modules/scan/compare_hash.py
+++ src/modules/scan/compare_hash.py
@@ -2,22 +2,21 @@
 # coding:utf-8
 
 from pymongo import MongoClient
-import os
+from get_hash import FileHash
 
 
 class VerifyHash(object):
 
     def __init__(self):
-        self.get_hash = None
+        self.file_hash = FileHash()
 
     @staticmethod
     def compare_hash(result_query: str, file_hash: str) -> str:
         """
         Compare hash in database
         """
-
         if not result_query:
-            assert ValueError(f"{file_hash} is not found")
+            raise ValueError(f"{file_hash} is not found")
         else:
             return "[>] Hash found ! Malware detected."
 
@@ -25,12 +24,12 @@ class VerifyHash(object):
         """
         Query to NoSQL database
         """
-        self.get_hash.FileHash.get_hash()
+        hash_value = self.file_hash.get_hash(file_hash)
 
-        mongo_client = MongoClient('127.0.0.1', 27027)
+        mongo_client = MongoClient('mongodb', 27027)
         mongo_db = mongo_client['whitestation_db']
 
         collection = mongo_db['hash_collection']
 
-        result_query = collection.find_one({"hash": file_hash})
+        result_query = collection.find_one({"hash": hash_value})
         return result_query
