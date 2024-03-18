6d4a0683d8f53be8ef135d42ca2aa18b8ce5e468
Isis <83175091+1sis@users.noreply.github.com>
2024-03-18 11:22:39 +0100

    [+] main & requirements corrected

--- src/main.py
+++ src/main.py
@@ -8,7 +8,6 @@ import os
 from modules.scan.get_hash import FileHash
 from modules.scan.compare_hash import VerifyHash
 from modules.yara.scan_yara import Scanner
-from modules.generate.create_pdf import PdfCreator
 
 
 def arg_parse():
@@ -21,17 +20,15 @@ def arg_parse():
     parser.add_argument('-d', '--directory', help="Provide a directory")
     parser.add_argument('-y', '--yara', action="store_true", help="Use YARA rules")
     parser.add_argument('-s', '--scan', action="store_true", help="Scan with Hashtable")
-    parser.add_argument('-e', '--export', action="store_true", help="Export the report in PDF")
     return parser.parse_args()
 
 
-def process_input(path, yara, scan, export):
+def process_input(path, yara, scan):
     """
     Process input
     :param path: Path to the file or dir
     :param yara: Scanning with Yara rules
     :param scan: Scanning with hashTable
-    :param export: Export in PDF the result
     """
     if os.path.isfile(path):
         if scan:
@@ -43,10 +40,6 @@ def process_input(path, yara, scan, export):
             print("[+] Yara rules will be used.")
             file_hash = FileHash().get_hash(path)
             return Scanner().scan_file(file_hash, path)
-
-        if export:
-            print("[+] Exporting the report in PDF.")
-            PdfCreator().generate_pdf()
 
     elif os.path.isdir(path):
         for root, dirs, files in os.walk(path):
@@ -62,10 +55,6 @@ def process_input(path, yara, scan, export):
                     file_hash = FileHash().get_hash(file_path)
                     # Scanner().scan_file(file_path, file_hash)
 
-                if export:
-                    print("[+] Exporting the report in PDF for:", file_path)
-                    PdfCreator().generate_pdf()
-
 
 def main(parser):
     """
@@ -74,10 +63,10 @@ def main(parser):
     :return:
     """
     if parser.file:
-        process_input(parser.file, parser.yara, parser.scan, parser.export)
+        process_input(parser.file, parser.yara, parser.scan)
 
     if parser.directory:
-        process_input(parser.directory, parser.yara, parser.scan, parser.export)
+        process_input(parser.directory, parser.yara, parser.scan)
 
 
 if __name__ == "__main__":
